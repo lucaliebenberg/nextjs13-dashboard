340f65194f53c1dff95267b97b5bc59819ecf41e
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-07-03 16:02:42 +0200

    updated project files + packages

--- nextjs13-dashboard/src/app/page.tsx
+++ nextjs13-dashboard/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 
+// import DataTable from "../components/ui/table";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-black">
       <h1>Home page</h1>
+      {/* <DataTable /> */}
     </main>
   );
 }
